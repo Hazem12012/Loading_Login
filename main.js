2a8951c9b5c564f3cf1857c7197947be34d076e8
hazem <hazemmahmoud12012@gmail.com>
2024-05-24 10:21:18 +0300

    End edit to project

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const button = document.querySelector(".btn");
-const AnimationB = document.querySelectorAll(".container span");
+const inputLap= document.querySelectorAll(".input-box label");
 const inputBox = document.querySelectorAll(".input-box input");
+const AnimationB = document.querySelectorAll(".container span");
 
-button.onclick = function () {
-  button.setAttribute("disabled", "");
 
-  inputBox.forEach(function (ele) {
-    ele.setAttribute("readonly", "");
-  });
-  AnimationB.forEach(function (ele) {
-    ele.classList = "clickAni";
-  });
-};
+  button.onclick = function () {
+    // disable button job after login
+    button.setAttribute("disabled", "");
+    // Style label after login
+    inputLap.forEach(function (ele) {ele.style.cssText = " top: 1px;font-size: 0.8em;background-color: #192f3a; padding: 0 4px;color: #0ef;";
+    });
+    // Disable writing input after login 
+    inputBox.forEach(function (ele) {
+      ele.setAttribute("readonly", "");
+    });
+    // animation after login
+    AnimationB.forEach(function (ele) {
+      ele.classList = "clickAni";
+    });
+  };
+
+
